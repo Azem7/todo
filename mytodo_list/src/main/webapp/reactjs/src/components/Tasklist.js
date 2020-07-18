65a4bb57d5c53845056aecfa7d2ea51b5edbe6fc
Mohamad Kattaa <53947632+MKattaa@users.noreply.github.com>
2020-07-18 09:48:58 +0200

    add show Tasklist (Tasklist.js)

--- mytodo_list/src/main/webapp/reactjs/src/components/Tasklist.js
+++ mytodo_list/src/main/webapp/reactjs/src/components/Tasklist.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
-import {Card, Table} from "react-bootstrap";
+import {Card, Table, ButtonGroup, Button} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faList} from "@fortawesome/free-solid-svg-icons";
+import {faEdit, faList, faTrash} from "@fortawesome/free-solid-svg-icons";
 import axios from 'axios';
 
 export default class Tasklist extends Component {
@@ -10,12 +10,15 @@ export default class Tasklist extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            books: []
+            tasks: []
         };
     }
 
     componentDidMount() {
-        axios.get("http://localhost:8080/tasks").then(response => console.log(response.data));
+        axios.get("http://localhost:8080/tasks")
+            .then(response => response.data).then((data) => {
+            this.setState({tasks: data});
+        });
     }
 
     render() {
@@ -28,14 +31,37 @@ export default class Tasklist extends Component {
                         <tr>
                             <th>Task</th>
                             <th>Description</th>
-                            <th>Due Date</th>
+                            <th>Deadline</th>
                             <th>Creation Date</th>
+                            <th>Actions</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr align="center">
-                            <td colSpan="6">No Tasks Available.</td>
-                        </tr>
+                        {
+                            this.state.tasks.length === 0 ?
+                                <tr align="center">
+                                    <td colSpan="6">No Tasks Available.</td>
+                                </tr> :
+                            this.state.tasks.map((task) => (
+                                <tr key={task.id}>
+                                    <td>{task.task}</td>
+                                    <td>{task.description}</td>
+                                    <td>{task.dueDate}</td>
+                                    <td>{task.creationDate}</td>
+                                    <td>
+                                        <ButtonGroup>
+                                            <Button size="sm" variant="outline-primary">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </Button>{' '}
+                                            <Button size="sm" variant="outline-danger">
+                                                <FontAwesomeIcon icon={faTrash} />
+                                            </Button>
+                                        </ButtonGroup>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+
                         </tbody>
                     </Table>
                 </Card.Body>
